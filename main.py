ccf697f6d118a8922e1693a52d476603f4d53aef
esadabdullahkosedag <kaesoftware@gmail.com>
2026-07-15 21:27:51 +0300

    chore: structured main.py file

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("labox first version")
+import os 
+import sys
+
+
+class Main():
+    def __init__(self, profile:dict):
+        self.profile = profile
+        self.run()
+
+    def run(self):
+        pass
+
+    
+if __name__ == "__main__":
+    main = Main({"key":"value"})
+
+
