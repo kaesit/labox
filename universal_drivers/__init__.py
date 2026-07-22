0c2d4d753372f3550b0af8452247aac0e95713d2
esadabdullahkosedag <kaesoftware@gmail.com>
2026-07-22 20:33:33 +0300

    refactor: Orgonised __init__.py of universal_drivers

--- universal_drivers/__init__.py
+++ universal_drivers/__init__.py
@@ -1,1 +1,39 @@
 import os
+import sys
+from mcu.esp32 import ESP32
+from mcu.jetson import Jetson
+from mcu.nxp import NXP
+from mcu.raspberry_pi import RaspberryPi
+from mcu.renesas import Renesas
+from mcu.stm32 import Nucleo,UnitBoards
+from transport.ble import BLE
+from transport.can import CAN
+from transport.ethernet import ETHERNET
+from transport.i2c import I2C
+from transport.modbus import MODBUS
+from transport.serial import SERIAL
+from transport.spi import SPI
+from transport.tcp import TCP
+from transport.UDP import UDP
+from transport.USB import USB
+
+__all__ =
+[
+        "ESP32",
+        "Jetson",
+        "NXP",
+        "RaspberryPi",
+        "Renesas",
+        "Nucleo",
+        "UnitBoards",
+        "BLE",
+        "CAN",
+        "ETHERNET",
+        "I2C",
+        "MODBUS",
+        "SERIAL",
+        "SPI",
+        "TCP",
+        "UDP",
+        "USB",
+]
