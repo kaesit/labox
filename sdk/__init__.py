069e89ccd37b47b5fc7b8373f0efcc8883684f29
esadabdullahkosedag <kaesoftware@gmail.com>
2026-07-22 20:39:12 +0300

    chore: edited __init__.py of SDK

--- sdk/__init__.py
+++ sdk/__init__.py
@@ -1,3 +1,12 @@
-import os
 import sys
-from fastapi import FastAPI
+from os.win32 import WIN32
+from os.osx import OSX
+from server.base import ServerModel
+
+
+__all__ = 
+[
+        "WIN32",
+        "OSX",
+        "ServerModel"
+]
