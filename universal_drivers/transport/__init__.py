89d638438931890a3baaca263f7664d9892609b2
esadabdullahkosedag <kaesoftware@gmail.com>
2026-07-22 22:42:10 +0300

    feat: wrote __init__.py file of universal_drivers/transport

---
+++ universal_drivers/transport/__init__.py
@@ -0,0 +1,25 @@
+from ble.core.base import BleModel
+from can.core.base import CanModel
+from ethernet.core.base import EthernetModel
+from i2c.core.base import I2CModel
+from modbus.core.base import ModBusModel
+from serial.core.base import SerialModel
+from spi.core.base import SPIModel
+from tcp.core.base import TCPModel
+from udp.core.base import UDPModel
+from usb.core.base import USBModel
+
+
+__all__ =
+[
+        "BleModel",
+        "CanModel",
+        "EthernetModel",
+        "I2CModel",
+        "ModBusModel",
+        "SerialModel",
+        "SPIModel",
+        "TCPModel",
+        "UDPModel",
+        "USBModel",
+]
