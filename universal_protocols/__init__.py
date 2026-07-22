ce6ad0070fbf57115f134beec7f9dd48cc6758b0
esadabdullahkosedag <kaesoftware@gmail.com>
2026-07-22 18:22:18 +0300

    feat: added class structure of Protocols
    Added class structure of Prtocols and it's get and allocation functions

--- universal_protocols/__init__.py
+++ universal_protocols/__init__.py
@@ -5,3 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PROTOCOLS = [protocol for protocol in os.getenv("PROTOCOLS").split(",")]
+MEMORY = virtual_memory
+
+class Protocols():
+    def __init__(self, protocols, memory):
+        self.protocols = PROTOCOLS
+        self.memory = MEMORY
+    # @Incomplete @Important Memory allocation is so important and it must not be forgotten 
+    def VirtualMemoryAllocation(self):
+        pass
+    # This function returns protocols value
+    def GetProtocolsList(self):
+        return self.protocols
+
