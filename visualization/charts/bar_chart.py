181d891b3c19f39b0886bfd14a9bd7c21329313a
esadabdullahkosedag <kaesoftware@gmail.com>
2026-07-24 16:03:45 +0300

    refactor: Completed BarChart Tool

--- visualization/charts/bar_chart.py
+++ visualization/charts/bar_chart.py
@@ -8,3 +8,16 @@ class BarChartVisualizer(BaseVisualizer):
         super().__init__(title=title, **kwargs)
         self.ax.set_xlabel(xlabel)
         self.ax.set_ylabel(ylabel)
+
+    def plot(self, data:Dict[str, float], color:str="#00ffcc"):
+        # @Incomplete
+        # @Scenario {'Nucleo_Basinc': 102.4, 'Valve_Flow'}
+        categories=list(data.keys())
+        values=list(data.keys())
+
+        # Draw bar graph (diverge from others with white corners)
+
+        # Data tags
+        for bar in bars:
+            height = bar.get_height()
+            self.ax.annotate(f'{height:.2f}', xy=(bar.get_x() + bar.get_width() / 2, height), xytext=(0, 5), textcoords="offset points", ha='center', va='bottom', color='#ffffff')
