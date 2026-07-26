60931d9e59375eaf895ed1e1e9006bdba214056f
esadabdullahkosedag <kaesoftware@gmail.com>
2026-07-26 19:03:39 +0300

    feat(storage): added __init__.py file of storage

---
+++ storage/__init_.py
@@ -0,0 +1,8 @@
+from core.base import BaseStorageModel
+from core.base import StorageOptimizer
+from core.base import DataCompressModel
+from core.base import DataDecompressionModel
+from core.base import BaseDataWriteModel
+from core.base import BaseDataReaderModel
+
+__all__ = ["BaseStorageModel", "StorageOptimize", "DataCompressModel", "DataDecompressionModel", "BaseDataWriteModel", "BaseDataReaderModel"]
