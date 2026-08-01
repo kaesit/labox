ef6aa7c468f4a701d7ae04bf0b47b4cfbad5b1e5
esadabdullahkosedag <kaesoftware@gmail.com>
2026-08-01 15:02:06 +0300

    init(plugins): Added __init__.py file of plugins module

---
+++ plugins/__init__.py
@@ -0,0 +1,22 @@
+from base import EmbeddedPlugins
+from base import QualityControlPlugins
+from base import ExperimentEnginePlugins
+from base import AutomationPlugins
+from base import StoragePlugins
+from base import SchedulerPlugins
+from base import SecurityPlugins
+from base import CliPlugins
+from base import SimulationPlugins
+
+
+__all__ = [
+        "EmbeddedPlugins",
+        "QualityControlPlugins",
+        "ExperimentEnginePlugins",
+        "AutomationPlugins",
+        "StoragePlugins",
+        "SchedulerPlugins",
+        "SecurityPlugins",
+        "CliPLugins",
+        "SimulationPlugins"
+]
