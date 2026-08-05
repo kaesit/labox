6cf9ebc277fd815e427e3ed04831dc08a0cfaf5e
esadabdullahkosedag <kaesoftware@gmail.com>
2026-08-05 22:36:08 +0300

    feat(quality_control): Added Enumarete Status class

--- quality_control/core/base.py
+++ quality_control/core/base.py
@@ -1,8 +1,22 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Callable, Any, Optional
+from enum import Enum
 
+class Status(Enum):
+        CONTROLING = "controling"
+        CHECKING = "checking"
+        VALIDATING = "validating"
+        ERROR = "error"
+        CANCELLED = "cancelled"
+        ACCEPTED = "accepted"
+        REJECTED = "rejected"
 
 class BaseQualityModel(ABC):
     def __init__(self) -> None:
         pass
+
+class QualityTask:
+    def __init__(self) -> Dict[str, Any]:
+        pass
+
 
