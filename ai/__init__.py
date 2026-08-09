430c8badea2b5549ce7b140067e52de615d36be4
esadabdullahkosedag <kaesoftware@gmail.com>
2026-08-09 21:58:53 +0300

    init(ai): Add init file of ai module

---
+++ ai/__init__.py
@@ -0,0 +1,13 @@
+from anomaly import AnomalyDetector
+from anomaly import AnomalyModel
+from forecast import ForecastModel
+from vision import VisionModel
+
+
+__all__ = [
+
+        "AnomalyDetector",
+        "AnomalyModel",
+        "ForecastModel",
+        "VisionModel"
+]
