837f4ccb2735ef32e9b3605a1e201924cd13b613
esadabdullahkosedag <kaesoftware@gmail.com>
2026-08-11 16:47:31 +0300

    refactor: Added new architectural notes and codes for AnomalyDetector class in ai module

--- ai/anomaly/base.py
+++ ai/anomaly/base.py
@@ -1,11 +1,21 @@
 import os, sys
+from typing import Dict, Optional, Any, 
+import time
 
+# @Metric, it is 120K miliseconds which equals to 2 minutes
+# 2 minutes is determined and final time for to spend to detect or diagnose each anomaly, this variable can be modified depends on amount of anomalies the platform deals with, but in use case of average 2 anomalies, should be detected/diagnosed maximumly in 4 minutes that is the limit 
+AVERAGE_TIME = 1200000
 
 class AnomalyDetector():
-    def __init__(self):
-        pass
+    def __init__(self, cases: Dict[str, Any]):
+        self.cases = cases
+        amnt_cases = len(self.cases) # This detects amount of cases to diagnose anomalies
+        print(amnt_cases)
+        # @Incomlpete, runner function is not completed, but i have to write a function that can run a lightweight anomaly detector model i have from a publication, it should be done maximumly in 4 minutes and output the results very clear and compact
+        run()
     
     def run(self):
+        """ This function will run the main diagnoses to detect potential anomalies happened by custom or third party models"""
         pass
 
     def fetch_anomaly_classes(self, handler, url:str):
