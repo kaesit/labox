eaa836512f169970fe7dc5c4859c2c1af41cf05a
esadabdullahkosedag <kaesoftware@gmail.com>
2026-08-11 13:43:10 +0300

    refactor: Edited __init__.py file of ai module to prevent potentail errors

--- ai/__init__.py
+++ ai/__init__.py
@@ -1,13 +1,19 @@
-from anomaly import AnomalyDetector
-from anomaly import AnomalyModel
-from forecast import ForecastModel
-from vision import VisionModel
+from .anomaly import *
+from .forecast import *
+from .optimization import *
+from .vision import *
 
+# @Incomplete i commented this lines to be a trigger to me for remembering what was the determined names of classes
+# @Must be revisited
+#from anomaly import AnomalyDetector
+#from anomaly import AnomalyModel
+#from forecast import ForecastModel
+#from vision import VisionModel
 
 __all__ = [
-
-        "AnomalyDetector",
-        "AnomalyModel",
-        "ForecastModel",
-        "VisionModel"
+    "anomaly",
+    "forecast",
+    "optimization",
+    "vision"
 ]
+
