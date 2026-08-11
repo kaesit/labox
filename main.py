d25f3ba23fc5c344cf98c730e4ffb21d88e8ff0f
esadabdullahkosedag <kaesoftware@gmail.com>
2026-08-11 21:39:53 +0300

    fix: deleted a wrong print line with a function in it
    Deleted unnecessary print statement that tries to run an async function in a sync function

--- main.py
+++ main.py
@@ -87,7 +87,6 @@ class Main():
 
     def run(self):
         print("UVICORN RUNS...")
-        print(getProfile())
         uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
         
     
