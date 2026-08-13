5795ea2abc79e043deae415fab369bcb3eda0fc1
esadabdullahkosedag <kaesoftware@gmail.com>
2026-08-13 14:48:31 +0300

    feat(simulations): Added a new simulation BaseSimulationModel class

---
+++ simulation/core/base.py
@@ -0,0 +1,26 @@
+import os, sys
+from abc import ABC, abstractmethod
+from typing import Dict, Any
+from enum import Enum
+
+class SimulationType(Enum):
+    CIRCUIT = 1
+    ROBOTIC = 2
+    STORAGE = 3
+    VISION = 4
+    SENSOR = 5
+
+
+class BaseSimulationModel(ABC):
+    def __init__(self, simulation_title: str = "System Simulation",
+                 simulation_type: SimulationType = SimulationType.CIRCUIT):
+        self.simulation_title = simulation_title
+        self.simulation_type = simulation_type
+        
+        self.is_running = False
+        self.results = {}
+    def start_simulation(self) -> None:
+        self.is_running = True
+        print(f"Starting {self.simulation_title}")
+
+
