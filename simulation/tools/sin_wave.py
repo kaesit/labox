ced79818a5ff4649a246476c0102e2cf771e87df
esadabdullahkosedag <kaesoftware@gmail.com>
2026-08-31 15:04:56 +0300

    feat: Add CRC8 Function
    Added a CRC8 calculator function for SensorPayload structure and added roadmap comments for next step

--- simulation/tools/sin_wave.py
+++ simulation/tools/sin_wave.py
@@ -1,4 +1,6 @@
 import numpy as np
+import zlib
+
 
 FREQUENCY = 200
 TIME_DURATION = 1.0
@@ -10,5 +12,29 @@ def generate_waves():
 
     return signal
 
+
+"""
+@Incomplete
+SensorPayload Structure
+    uint8_t sensor_id 1 byte max, 255 sensor
+    uint8_t status_flags 1 byte bitmask
+    uint16_t value 2 bytes
+    @Completed uint8_t crc 1 byte checksum
+
+    Crc function is written, next step is writing data encoding for RingBuffer
+    And using 5 BYTES MAXIMUM @Max
+"""
+
+def calculate_crc8(data: bytes) -> int:
+    crc = 0x00
+    for byte in data:
+        crc ^= byte
+        for _ in range (8):
+            if crc & 0x80:
+                crc = (crc << 1) ^ (0x07)
+            else:
+                crc <<= 1
+            crc &= 0xFF
+    return crc
 
 generate_waves()
